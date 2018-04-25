45f94d4cab73c6a8ace22b5c376ac6d51e365211
Andrei Haidu <andrei.haidu@yahoo.com>
2018-04-25 19:25:32 +0200

    dev branch for semlog 2

--- Source/RobCoG/RobCoG.Build.cs
+++ Source/RobCoG/RobCoG.Build.cs
@@ -8,7 +8,7 @@ public class RobCoG : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UROSBridge", "URoboSim", "Json", "JsonUtilities" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Json", "JsonUtilities" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {
             });
